ef1118f062fd229c55defe5bb4addc0676937256
Sarkis Varozian <svarozian@gmail.com>
2018-07-06 00:04:47 -0700

    add output description

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,9 @@
 output "aws_appautoscaling_policy_up_arn" {
-  value = "${aws_appautoscaling_policy.up.arn}"
+  description = "ARN of the scale up policy."
+  value       = "${aws_appautoscaling_policy.up.arn}"
 }
 
 output "aws_appautoscaling_policy_down_arn" {
-  value = "${aws_appautoscaling_policy.down.arn}"
+  description = "ARN of the scale down policy."
+  value       = "${aws_appautoscaling_policy.down.arn}"
 }
