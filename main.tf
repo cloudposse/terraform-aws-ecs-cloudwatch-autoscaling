aeca803202438f5abd07c0cb7be0a91654bae5e5
aknysh <andriy.knysh@gmail.com>
2019-10-30 16:27:45 -0400

    Convert to TF 0.12. Add tests. Add Codefresh test pipeline

--- main.tf
+++ main.tf
@@ -1,66 +1,68 @@
 module "scale_up_label" {
-  source     = "git::https://github.com/cloudposse/terraform-terraform-label.git?ref=tags/0.1.3"
-  name       = "${var.name}"
-  namespace  = "${var.namespace}"
-  stage      = "${var.stage}"
-  attributes = "${compact(concat(var.attributes, list("up")))}"
+  source     = "github.com/cloudposse/terraform-null-label.git?ref=0.16.0"
+  enabled    = var.enabled
+  name       = var.name
+  namespace  = var.namespace
+  stage      = var.stage
+  attributes = compact(concat(var.attributes, ["up"]))
+  delimiter  = var.delimiter
+  tags       = var.tags
 }
 
 module "scale_down_label" {
-  source     = "git::https://github.com/cloudposse/terraform-terraform-label.git?ref=tags/0.1.3"
-  name       = "${var.name}"
-  namespace  = "${var.namespace}"
-  stage      = "${var.stage}"
-  attributes = "${compact(concat(var.attributes, list("down")))}"
-}
-
-locals {
-  enabled = "${var.enabled == "true" ? 1 : 0}"
+  source     = "github.com/cloudposse/terraform-null-label.git?ref=0.16.0"
+  enabled    = var.enabled
+  name       = var.name
+  namespace  = var.namespace
+  stage      = var.stage
+  attributes = compact(concat(var.attributes, ["down"]))
+  delimiter  = var.delimiter
+  tags       = var.tags
 }
 
 resource "aws_appautoscaling_target" "default" {
-  count              = "${local.enabled}"
+  count              = var.enabled ? 1 : 0
   service_namespace  = "ecs"
   resource_id        = "service/${var.cluster_name}/${var.service_name}"
   scalable_dimension = "ecs:service:DesiredCount"
-  min_capacity       = "${var.min_capacity}"
-  max_capacity       = "${var.max_capacity}"
+  min_capacity       = var.min_capacity
+  max_capacity       = var.max_capacity
 }
 
 resource "aws_appautoscaling_policy" "up" {
-  count              = "${local.enabled}"
-  name               = "${module.scale_up_label.id}"
+  count              = var.enabled ? 1 : 0
+  name               = module.scale_up_label.id
   service_namespace  = "ecs"
   resource_id        = "service/${var.cluster_name}/${var.service_name}"
   scalable_dimension = "ecs:service:DesiredCount"
 
   step_scaling_policy_configuration {
     adjustment_type         = "ChangeInCapacity"
-    cooldown                = "${var.scale_up_cooldown}"
+    cooldown                = var.scale_up_cooldown
     metric_aggregation_type = "Average"
 
     step_adjustment {
       metric_interval_lower_bound = 0
-      scaling_adjustment          = "${var.scale_up_adjustment}"
+      scaling_adjustment          = var.scale_up_adjustment
     }
   }
 }
 
 resource "aws_appautoscaling_policy" "down" {
-  count              = "${local.enabled}"
-  name               = "${module.scale_down_label.id}"
+  count              = var.enabled ? 1 : 0
+  name               = module.scale_down_label.id
   service_namespace  = "ecs"
   resource_id        = "service/${var.cluster_name}/${var.service_name}"
   scalable_dimension = "ecs:service:DesiredCount"
 
   step_scaling_policy_configuration {
     adjustment_type         = "ChangeInCapacity"
-    cooldown                = "${var.scale_down_cooldown}"
+    cooldown                = var.scale_down_cooldown
     metric_aggregation_type = "Average"
 
     step_adjustment {
       metric_interval_upper_bound = 0
-      scaling_adjustment          = "${var.scale_down_adjustment}"
+      scaling_adjustment          = var.scale_down_adjustment
     }
   }
 }
