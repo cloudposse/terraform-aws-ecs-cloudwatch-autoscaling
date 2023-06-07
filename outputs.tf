b9bc73bc772d5436ee26d1f0c4eae6efbcc14d18
Max Lobur <max_lobur@outlook.com>
2023-06-07 19:40:33 +0300

    Support AWS Provider V5

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "scale_up_policy_arn" {
   description = "ARN of the scale up policy"
-  value       = join("", aws_appautoscaling_policy.up.*.arn)
+  value       = join("", aws_appautoscaling_policy.up[*].arn)
 }
 
 output "scale_down_policy_arn" {
   description = "ARN of the scale down policy"
-  value       = join("", aws_appautoscaling_policy.down.*.arn)
+  value       = join("", aws_appautoscaling_policy.down[*].arn)
 }
