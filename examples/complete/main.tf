2edbb9797c81fd32b79b22e852f40a7e48bfe5ee
Igor Rodionov <goruha@gmail.com>
2024-07-02 20:32:13 +0300

    Update main.tf

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 module "vpc" {
   source     = "cloudposse/vpc/aws"
-  version    = "0.18.0"
+  version    = "2.1.1"
   cidr_block = var.vpc_cidr_block
   context    = module.this.context
 }
